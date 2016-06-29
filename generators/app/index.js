c3c031a0b24d430d2433f63cd7968f12f8f031ab
涂涂 <hltx@hanlongtianxing.local>
2016-06-29 15:34:23 +0800

    完成

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,37 +4,35 @@ var chalk = require('chalk');
 var yosay = require('yosay');
 var mkdirp = require('mkdirp');
 var path = require('path');
-
-
 module.exports = yeoman.Base.extend({
-  constructor: function() {
+  constructor: function () {
     yeoman.Base.apply(this, arguments);
   },
-  //初始化方法
-  initializing: function() {
+  // 初始化方法
+  initializing: function () {
 
   },
-  //获取用户选项
-  prompting: function() {
+  // 获取用户选项
+  prompting: function () {
     var done = this.async();
     // Have Yeoman greet the user.
     this.log(yosay(
       'Welcome to the sensational ' + chalk.red('generator-website-app') + ' generator!'
     ));
     this.name = path.basename(process.cwd());
+    this.version = '1.0.0';
     this.license = 'ISC';
     this.description = '';
-    this.author = 'tutu';
 
     var prompts = [{
       type: 'input',
       name: 'name',
-      message: 'name of app',
+      message: '项目名称:',
       default: this.name
     }, {
       type: 'input',
       name: 'description',
-      message: 'description:',
+      message: '项目描述:',
       default: this.description
     }, {
       type: 'input',
@@ -43,21 +41,21 @@ module.exports = yeoman.Base.extend({
       default: this.license
     }, {
       type: 'input',
-      name: 'author',
-      message: 'author:',
-      default: this.author
+      name: 'version',
+      message: '项目版本:',
+      default: this.version
     }];
 
-    return this.prompt(prompts).then(function(props) {
+    return this.prompt(prompts).then(function (props) {
       this.name = props.name;
-      this.pkgName = props.name;
+      this.projectname = props.name;
       this.license = props.license;
-      this.author = props.author;
+      this.version = props.version;
       this.description = props.description;
-      done(); //进入下一个生命周期阶段
+      done(); // 进入下一个生命周期阶段
     }.bind(this));
   },
-  //保存配置
+  // 保存配置
   // configuring: function() {
 
   // },
@@ -65,10 +63,10 @@ module.exports = yeoman.Base.extend({
   // default: function() {
 
   // },
-  //写generator特殊文件（路由、控制器）
-  writing: function() {
-    //生成目录结构阶段
-    //默认源目录就是生成器的templates目录，目标目录就是执行`yo example`时所处的目录。调用this.template用Underscore模板语法去填充模板文件
+  // 写generator特殊文件（路由、控制器）
+  writing: function () {
+    // 生成目录结构阶段
+    // 默认源目录就是生成器的templates目录，目标目录就是执行`yo example`时所处的目录。调用this.template用Underscore模板语法去填充模板文件
     this.template('package.json', 'package.json');
     this.template('gulpfile.js', 'gulpfile.js');
     mkdirp('src');
@@ -79,14 +77,13 @@ module.exports = yeoman.Base.extend({
     mkdirp('src/view');
     this.copy('src/view/index.html', 'src/view/index.html');
     this.copy('src/scss/base.scss', 'src/scss/base.scss');
-
   },
-  //冲突后处理方案
+  // 冲突后处理方案
   // conflicts: function() {
 
   // },
-  //安装
-  install: function() {
+  // 安装
+  install: function () {
     var done = this.async();
     done();
     // this.spawnCommand('npm', ['install']) //安装项目依赖
@@ -99,12 +96,10 @@ module.exports = yeoman.Base.extend({
     //   })
     //   .on('error', done);
   },
-  //安装结束
-  end: function() {
+  // 安装结束
+  end: function () {
     var done = this.async();
     done();
     this.log('工作流初始化完毕, 请 `npm install` 安装依赖.');
-
-
   }
 });
--- generators/app/templates/gulpfile.js
+++ generators/app/templates/gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp'),
   clean = require('del'),
   gulpif = require('gulp-if'),
-  sass = require('gulp-ruby-sass'),
+  sass = require('gulp-sass'),
   uglify = require('gulp-uglify'),
   postcss = require('gulp-postcss'),
   // replace = require('gulp-replace'),
@@ -15,8 +15,8 @@ var cssnano = require('cssnano');
 var reload = browserSync.reload;
 var tmtsprite = require('gulp-tmtsprite'); // css sprites图片
 var zip = require('gulp-zip');
-//解析 scss 文件
-gulp.task('scss', function() {
+// 解析 scss 文件
+gulp.task('scss', function () {
   var processors = [
     autoprefixer,
     cssnano,
@@ -28,50 +28,49 @@ gulp.task('scss', function() {
     .pipe(postcss(processors))
     .pipe(gulp.dest('./src/css'));
 });
-//解析 scss 文件
-gulp.task('css', function() {
+// 解析 scss 文件
+gulp.task('css', function () {
   return gulp.src(['./src/css/*.css'])
     .pipe(gulp.dest('./dist/css'));
 });
-//监听 css 和 css sprite 生成
-gulp.task('watchScss', function() {
+// 监听 css 和 css sprite 生成
+gulp.task('watchScss', function () {
   // gulp.watch('./src/images/sprite/**', ['sprites']);
   gulp.watch('./src/scss/*.scss', ['scss']);
 });
 
-gulp.task('images', function() {
+gulp.task('images', function () {
   return gulp.src(['./src/images/*.{gif,jpg,png,ico}'])
     .pipe(gulp.dest('./dist/images'));
 });
-gulp.task('js', function() {
+gulp.task('js', function () {
   return gulp.src(['./src/js/*.js'])
     // .pipe(uglify())
     .pipe(gulp.dest('./dist/js/'));
 });
-gulp.task('plugin', function() {
+gulp.task('plugin', function () {
   return gulp.src(['./src/lib/**'])
     .pipe(gulp.dest('./dist/lib/'));
 });
-gulp.task('html', ['css', 'images', 'js', 'plugin'], function() {
+gulp.task('html', ['css', 'images', 'js', 'plugin'], function () {
   return gulp.src(['./src/view/*.html'])
     .pipe(gulp.dest('./dist/views'));
 });
-
 gulp.task('del', clean.bind(null, ['dist']));
 // 预设任务
-gulp.task('default', ['html'], function() {});
-//生产预览
-gulp.task('serve', ['scss', 'watchScss'], function() { //注册任务
-  browserSync({ //调用API
-    files: "**",
+gulp.task('default', ['html'], function () { });
+// 生产预览
+gulp.task('serve', ['scss', 'watchScss'], function () { // 注册任务
+  browserSync({ // 调用API
+    files: '**',
     port: 8000,
     server: {
-      baseDir: ['./src'] //监听当前路径
+      baseDir: ['./src'] // 监听当前路径
     },
     startPath: 'view/index.html'
   });
 });
-gulp.task('zip', function() {
+gulp.task('zip', function () {
   return gulp.src('dist/**/*')
     .pipe(zip('dist.zip'))
     .pipe(gulp.dest('./'));
